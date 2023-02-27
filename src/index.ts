81a3fc985d2fc1b41725939880fb00c0b4ccd415
Elio Struyf <elio@struyfconsulting.be>
2023-02-27 13:58:00 +0100

    Added new register functions

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,88 @@
 declare global {
   interface Window {
     fmExternal: {
-      getCardFooter: (filePath: string, data: any) => Promise<string>;
+      getCustomFields: {
+        name: string;
+        html: (
+          data: any,
+          change: (value: any) => void
+        ) => Promise<{ title: string; content: string } | undefined>;
+      }[];
+      getPanelView: (data: any) => Promise<PanelViewResult | undefined>;
+      getCardImage: (
+        filePath: string,
+        data: any
+      ) => Promise<string | undefined>;
+      getCardFooter: (
+        filePath: string,
+        data: any
+      ) => Promise<string | undefined>;
     };
   }
 }
 
+// Interfaces
+
+export interface PanelViewResult {
+  title: string;
+  content: string;
+}
+
+// Dashboard
+
+/**
+ * Register a card image renderer
+ * @param cb
+ */
+export const registerCardImage = (
+  cb: (filePath: string, metadata: any) => Promise<string>
+) => {
+  window.fmExternal = window.fmExternal || {};
+
+  window.fmExternal.getCardImage = cb;
+};
+
+/**
+ * Register a card footer renderer
+ * @param cb
+ */
 export const registerCardFooter = (
-  cb: (filePath: string, data: any) => Promise<string>
+  cb: (filePath: string, metadata: any) => Promise<string>
 ) => {
   window.fmExternal = window.fmExternal || {};
 
   window.fmExternal.getCardFooter = cb;
+};
+
+// Panel
+
+/**
+ * Register a panel view renderer
+ * @param cb
+ */
+export const registerPanelView = (
+  cb: (metadata: any) => Promise<PanelViewResult | undefined>
+) => {
+  window.fmExternal = window.fmExternal || {};
+
+  window.fmExternal.getPanelView = cb;
+};
+
+/**
+ * Register a custom field
+ * @param name
+ * @param cb
+ */
+export const registerCustomFields = (name: string, cb: any) => {
+  window.fmExternal = window.fmExternal || {};
+  window.fmExternal.getCustomFields = window.fmExternal.getCustomFields || [];
+
+  const fieldRef = window.fmExternal.getCustomFields.find(
+    (x) => x.name === name
+  );
+  if (fieldRef) {
+    fieldRef.html = cb;
+  } else {
+    window.fmExternal.getCustomFields.push({ name, html: cb });
+  }
 };
